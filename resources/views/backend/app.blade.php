e2cceba7bc1693de3341447e2bbc9e580d99b249
karabayyazilim <45515180+karabayyazilim@users.noreply.github.com>
2020-05-25 00:14:26 +0300

    karabayyazilim

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -33,7 +33,7 @@
         <a href="index3.html" class="brand-link">
             <img src="{{$site_image}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <span class="brand-text font-weight-light">{{$title}}</span>
         </a>
 
         <div class="sidebar">
